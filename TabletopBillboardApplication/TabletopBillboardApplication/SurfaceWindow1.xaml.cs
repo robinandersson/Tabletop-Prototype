d74a0c78a6591ec5daee1edc7a6be4396beab615
Sigrid Speckens <x_sigi_x@hotmai.com>
2015-05-20 20:19:15 +0200

    ImageData
    Class ImageData plus loading the data from a file

--- TabletopBillboardApplication/TabletopBillboardApplication/SurfaceWindow1.xaml.cs
+++ TabletopBillboardApplication/TabletopBillboardApplication/SurfaceWindow1.xaml.cs
@@ -39,6 +39,7 @@ namespace TabletopBillboardApplication
             rnd = new Random();
             // load image
             LoadImages();
+            LoadText();
 
             foreach (object obj in scatter.Items)
             {
@@ -70,7 +71,31 @@ namespace TabletopBillboardApplication
             ApplicationServices.WindowInteractive += OnWindowInteractive;
             ApplicationServices.WindowNoninteractive += OnWindowNoninteractive;
             ApplicationServices.WindowUnavailable += OnWindowUnavailable;
+            //ManipulationDelta += OnManipulationDelta;
         }
+
+        /*public void OnManipulationDelta(object sender, ManipulationDeltaEventArgs args)
+        {
+            if (sender.GetType() == typeof(ScatterViewItem))
+            {
+                ScatterViewItem name = (ScatterViewItem) (sender);
+
+                if (name.ActualHeight > 700 || name.ActualWidth > 700)
+                {
+                    bool W = name.ActualHeight == Math.Max(name.ActualHeight, name.ActualWidth);
+                    if (W)
+                    {
+                        name.Height = 700;
+                    }
+                    else
+                    {
+                        name.Width = 700;
+                    }
+                }
+            }
+            //svi.MaxHeight = 700;
+            //svi.MaxWidth = 700;
+        }*/
 
         /// <summary>
         /// Removes handlers for window availability events.
@@ -82,6 +107,7 @@ namespace TabletopBillboardApplication
             ApplicationServices.WindowNoninteractive -= OnWindowNoninteractive;
             ApplicationServices.WindowUnavailable -= OnWindowUnavailable;
         }
+
 
         /// <summary>
         /// This is called when the user can interact with the application's window.
@@ -133,6 +159,7 @@ namespace TabletopBillboardApplication
                 svi.Height = scale*dice;
                 //svi.Width = img.Source.Height * dice; // Way too large
                 //svi.Height = img.Source.Width * dice; // Way too large
+                //int longestEdge = int (Math.Max(img.Source.Height, img.Source.Width));
 
                 scatter.Items.Add(svi);
             }
@@ -141,18 +168,75 @@ namespace TabletopBillboardApplication
         // load text from source
         void LoadText()
         {
-            try
+
+            // TODO: read it in via Tags class
+            //string envDir = Environment.CurrentDirectory;
+            //string[] fileNames = Directory.GetFiles(envDir + @"\Resources\Posters", "*.jpg");
+            ImageData[] data= null;// = new ImageData();
+            try //TODO
             {
-                using (StreamReader sr = new StreamReader("A3 euromath poster.txt"))
+                using (StreamReader sr = new StreamReader(@"Resources\Text Posters\TextImage.txt"))
                 {
-                    String line = sr.ReadToEnd();
-                    Console.WriteLine(line);
+                    String name = sr.ReadLine();
+                    int num = 0;
+                    while (name != null)
+                    {
+                        String dat = sr.ReadLine();
+                        DateTime date = Convert.ToDateTime(dat);
+                        int num2 = 0;
+                        List<String> tag= new List<String>();
+                        String tag0 = sr.ReadLine(); 
+                        while (tag0 != "$")
+                        {
+                            tag.Add(tag0);
+                            tag0 = sr.ReadLine();
+                        }
+                        String line0 = sr.ReadLine();
+                        String line = null;
+                        while (line0 != "$")
+                        {
+                            line = String.Concat(line,line0);
+                            line0 = sr.ReadLine();
+                        }
+                        data[num] = new ImageData(name, date, tag, line);
+                        num++;
+                    }
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
+            }
+        }
+
+        private class ImageData
+        {
+            private List<String> Tags;
+            private String Text, Name;
+            private DateTime Date;
+
+            public ImageData(String Name0, DateTime Date0, List<String> Tags0, String Text0){
+                Name = Name0;
+                Date = Date0;
+                Tags = Tags0;
+                Text = Text0;
+            }
+
+            private string getName(){
+                return Name;
+            }
+
+            private DateTime getDate(){
+                return Date;
+            }
+
+            private List<string> getTags(){
+                return Tags;
+            }
+
+            private string getText(){
+                return Text;
             }
         }
     }
